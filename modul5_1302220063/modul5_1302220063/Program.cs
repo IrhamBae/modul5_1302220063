64661dffea0b181ac35ab1be6ac45e9547fd5547
Irham Baehaqi <149279763+IrhamBae@users.noreply.github.com>
2024-03-23 08:15:21 +0700

    Add files via upload
    "first commit"

---
+++ modul5_1302220063/modul5_1302220063/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace modul5_1302220063
+{
+    internal class Penjumlahan
+    {
+        public static void Main(String[] args)
+        {
+                
+        }
+    }
+}
